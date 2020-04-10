25636a7470992698f368b9a30efe8dbd65353858
sosa4life <sh.sosa@gmail.com>
2020-04-10 17:38:21 +0300

    start

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Use this for initialization
+    void Start () {
+        PathFinder pathfinder = FindObjectOfType<PathFinder>();
+        var path = pathfinder.GetPath();
+        StartCoroutine(FollowPath(path));
+    }
+
+    IEnumerator FollowPath(List<Waypoint> path)
+    {
+        print("Starting patrol..."); 
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+    }
+}
---
+++ Assets/Waypoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public ok here as is a data class
+    public bool isExplored = false;
+    public Waypoint exploredFrom;
+
+    Vector2Int gridPos;
+
+    const int gridSize = 10;
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+	
+    // consider setting own color in Update()
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x ),
+            Mathf.RoundToInt(transform.position.z )
+        );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("top").GetComponent<MeshRenderer>();
+        topMeshRenderer.material.color = color;
+    }
+}
+
